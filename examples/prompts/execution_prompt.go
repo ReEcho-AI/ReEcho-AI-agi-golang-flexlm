b5a0440f2fecc6ff487bf3f7050eb257cdd4819d
ReEcho-AI <158334832+ReEcho-AI@users.noreply.github.com>
2022-04-17 17:52:55 +0000

    Update Polish translation.

--- examples/prompts/execution_prompt.go
+++ examples/prompts/execution_prompt.go
@@ -22,3 +22,25 @@ var (
 		`Execute the task (with the given context) to archieve the objective. Output the result of the task to result text.`,
 		prompt.NewExample(
 			&ExecutionInput{
+				Objective:   Objective("original objective"),
+				CurrentTask: Task{Name: "current task"},
+				SolvedTasks: Tasks{
+					{Name: "solved task 1"},
+					{Name: "solved task 2"},
+				},
+				RelevantContext: []TaskContext{
+					{
+						Text: "context text 1",
+					},
+				},
+			},
+			&ExecutionOutput{
+				CurrentTaskResult: TaskResult{
+					Task:       Task{Name: "current task"},
+					ResultText: "result text",
+				},
+			},
+		),
+		nil,
+	)
+)
