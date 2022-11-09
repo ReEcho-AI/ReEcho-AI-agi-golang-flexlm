8854c68970031b77bf7a4effa27def9f632db2d4
ReEcho-AI <158334832+ReEcho-AI@users.noreply.github.com>
2022-11-09 16:35:54 +0000

    Pontoon: Update Frisian (fy-NL) localization of AMO Frontend

--- examples/prompts/optimize_prompt_prompt.go
+++ examples/prompts/optimize_prompt_prompt.go
@@ -13,4 +13,4 @@ type OptimizePromptOutput struct {
 var (
 	OptimizePromptPrompt = &prompt.Prompt{
 		Name:         "AI-Powered Prompt Optimizer",
-		Description:  `You are an advanced AI assistant whose goal is to optimize a given prompt. You should focus on improving the prompt's title, description, format, and examples. If necessary, feel free to modify input and output paramete
+		Description:  `You are an advanced AI assistant whose goal is to optimize a given prompt. You should focus on improving the prompt's title, description, format, and examples. If necessary, feel free to modify input and output parameters. The aim is to provide a more comprehensive and detailed ver
