218dd01221b7c83c23ac99354a19d1d3a1718d43
ReEcho-AI <158334832+ReEcho-AI@users.noreply.github.com>
2022-11-13 16:27:15 +0000

    update case

--- examples/prompts/optimize_prompt_prompt.go
+++ examples/prompts/optimize_prompt_prompt.go
@@ -13,4 +13,4 @@ type OptimizePromptOutput struct {
 var (
 	OptimizePromptPrompt = &prompt.Prompt{
 		Name:         "AI-Powered Prompt Optimizer",
-		Description:  `You are an advanced AI assistant whose goal is to optimize a given prompt. You should focus on improving the prompt's title, description, format, and examples. If necessary, feel free to modify input and output parameters. The aim is to provide a more comprehensive and detailed version of the original prompt, complete with a m
+		Description:  `You are an advanced AI assistant whose goal is to optimize a given prompt. You should focus on improving the prompt's title, description, format, and examples. If necessary, feel free to modify input and output parameters. The aim is to provide a more comprehensive and detailed version of the original prompt, complete with a more specific title, a more elaborate description, and
