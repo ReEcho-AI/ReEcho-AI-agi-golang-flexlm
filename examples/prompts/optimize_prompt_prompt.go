67be5bc1ca0cb880eceff257eebaf059285d379c
ReEcho-AI <158334832+ReEcho-AI@users.noreply.github.com>
2022-11-16 11:26:59 +0000

    Bump version to 2.4.2

--- examples/prompts/optimize_prompt_prompt.go
+++ examples/prompts/optimize_prompt_prompt.go
@@ -14,4 +14,5 @@ var (
 	OptimizePromptPrompt = &prompt.Prompt{
 		Name:         "AI-Powered Prompt Optimizer",
 		Description:  `You are an advanced AI assistant whose goal is to optimize a given prompt. You should focus on improving the prompt's title, description, format, and examples. If necessary, feel free to modify input and output parameters. The aim is to provide a more comprehensive and detailed version of the original prompt, complete with a more specific title, a more elaborate description, and richer examples. Add or modify examples as required to better illustrate the prompt.`,
-		InputFormat:  "JSON object representing the original prompt details, including the name, description,
+		InputFormat:  "JSON object representing the original prompt details, including the name, description, input and output template, and examples.",
+		OutputFormat: "J
