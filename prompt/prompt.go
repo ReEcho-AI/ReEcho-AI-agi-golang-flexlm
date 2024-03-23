5bd97de19693799a31442c61fe512bb1af797f45
ReEcho-AI <158334832+ReEcho-AI@users.noreply.github.com>
2024-03-23 16:59:16 +0000

    Reverted last commit

--- prompt/prompt.go
+++ prompt/prompt.go
@@ -27,3 +27,66 @@ type Prompt struct {
 	Template     *Example `json:"template"`
 	Examples     Examples `json:"examples"`
 }
+
+func NewPrompt(name string, description string, template *Example, examples []Example) *Prompt {
+	return &Prompt{
+		Name:        name,
+		Description: description,
+		Template:    template,
+		Examples:    examples,
+	}
+}
+
+func NewSimplePrompt(name string, description string, input string, output string) *Prompt {
+	return NewPrompt(name, description, NewExample(input, output), nil)
+}
+
+func (c *Prompt) Format(input Input) (string, error) {
+	formattedInput, err := toJson(input)
+	if err != nil {
+		return "", err
+	}
+	formattedTemplate, err := c.Template.Format()
+	if err != nil {
+		return "", err
+	}
+	formattedExamples, err := c.Examples.Format()
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf(`You are an AI named "%s".
+%s
+
+Output a JSON-formatted string without outputting any other strings.
+
+Template:
+%s
+
+%s
+
+Input: %s
+Output:`, c.Name, c.Description, formattedTemplate, formattedExamples, formattedInput), nil
+}
+
+type Example struct {
+	Input  Input  `json:"input"`
+	Output Output `json:"output"`
+}
+
+func NewExample(input Input, output Output) *Example {
+	return &Example{
+		Input:  input,
+		Output: output,
+	}
+}
+
+func (p *Example) Format() (string, error) {
+	input, err := toJson(p.Input)
+	if err != nil {
+		return "", err
+	}
+
+	output, err := toJson(p.Output)
+	if err != nil {
+		return "", err
